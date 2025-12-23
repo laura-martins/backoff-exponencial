81829a4741cadccdcf303da39f1b6d82211484d7
Laura Martins <lauraga452@gmail.com>
2025-12-22 23:23:27 -0300

    feat(main): ajustado main para executar aplicação

--- src/main/kotlin/BackoffExponencialMain.kt
+++ src/main/kotlin/BackoffExponencialMain.kt
@@ -1,5 +1,11 @@
 package br.com.backoff.exponencial
 
-fun main() {
-    println("Hello World!")
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackoffExponencialApplication
+
+fun main(args: Array<String>) {
+    runApplication<BackoffExponencialApplication>(*args)
 }
