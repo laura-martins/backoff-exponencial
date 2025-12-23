7c305bba3ce5dd3c10c1e885a2e8f5bc80502a80
Laura Martins <lauraga452@gmail.com>
2025-12-23 02:54:13 -0300

    feat(sqs): refatorado consumer

--- src/main/kotlin/consumers/ConsumerSQS.kt
+++ src/main/kotlin/consumers/ConsumerSQS.kt
@@ -2,20 +2,44 @@ package br.com.backoff.exponencial.consumers
 
 import io.awspring.cloud.sqs.annotation.SqsListener
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
+import software.amazon.awssdk.services.sqs.model.Message
 
 @Component
-class ConsumerSQS {
-    private val log = LoggerFactory.getLogger(ConsumerSQS::class.java)
+class ConsumerSQS(
+    private val backoff: ExponentialBackoffAdapter
+) {
+
+    @Value("\${aws.sqs.queue.url.test-name}")
+    lateinit var queueUrl: String
 
     @SqsListener("\${aws.sqs.queue.test-name}")
-    fun consume(messageBody: String) {
+    fun consume(
+        messageBody: Message
+    ) {
         try {
-            log.info("[ConsumerSQS] Received message body={}", messageBody)
-            messageBody.toInt()
+            logger.info("[ConsumerSQS] Received message body={}", messageBody)
+            val body = messageBody.body()
+            body.toInt() // Simulate processing that may fail
         } catch (e: Exception) {
-            log.error("[ConsumerSQS] Error processing message", e)
+            logger.error("[ConsumerSQS] Error processing message", e)
+
+            val receiptHandle = messageBody.receiptHandle()
+            val receiveCount = messageBody.attributesAsStrings()["ApproximateReceiveCount"]
+                ?.toInt()
+
+            backoff.applyBackoff(
+                queueUrl = queueUrl,
+                receiptHandle = receiptHandle,
+                receiveCount = requireNotNull(receiveCount)
+            )
+
             throw e
         }
+    }
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(ConsumerSQS::class.java)
     }
 }
