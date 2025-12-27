5275a4cf8e2e1cc6a83d6b73b621dfd83001ad9f
Laura Martins <lauraga452@gmail.com>
2025-12-27 13:59:13 -0300

    feat(sqs): ajustado configuração deserialização

---
+++ src/main/kotlin/configurations/JacksonConfiguration.kt
@@ -0,0 +1,24 @@
+package br.com.backoff.exponencial.configurations
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.converter.MappingJackson2MessageConverter
+
+@Configuration
+class JacksonConfig {
+
+    @Bean
+    fun objectMapper(): ObjectMapper =
+        jacksonObjectMapper().registerKotlinModule()
+
+    @Bean
+    fun mappingJackson2MessageConverter(objectMapper: ObjectMapper): MappingJackson2MessageConverter {
+        val converter = MappingJackson2MessageConverter()
+        converter.objectMapper = objectMapper
+        converter.isStrictContentTypeMatch = false
+        return converter
+    }
+}
