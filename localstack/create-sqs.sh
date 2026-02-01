290df6270fb59210927a9285ac7d8aba57edab86
Laura Martins <lauraga452@gmail.com>
2026-02-01 00:50:49 -0300

    feat(sqs): ajustado configuração da DLQ

--- localstack/create-sqs.sh
+++ localstack/create-sqs.sh
@@ -8,7 +8,7 @@ awslocal sqs create-queue --queue-name test-queue-DLQ
 awslocal sqs set-queue-attributes \
     --queue-url http://sqs.sa-east-1.localhost.localstack.cloud:4566/000000000000/test-queue \
     --attributes '{
-    "RedrivePolicy": "{\"deadLetterTargetArn\":\"arn:aws:sqs.sa-east-1:000000000000:test-queue-DLQ\",\"maxReceiveCount\":\"3\"}"
+    "RedrivePolicy": "{\"deadLetterTargetArn\":\"arn:aws:sqs:sa-east-1:000000000000:test-queue-DLQ\",\"maxReceiveCount\":\"10\"}"
 }'
 
 echo "Filas SQS criadas com sucesso"
