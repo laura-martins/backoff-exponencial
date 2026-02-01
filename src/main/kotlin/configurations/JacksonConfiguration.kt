5a682b29d761238ed99fa6332e412d465915498e
Laura Martins <lauraga452@gmail.com>
2026-02-01 00:25:43 -0300

    feat(sqs): ajustado config do michel jackson

--- src/main/kotlin/configurations/JacksonConfiguration.kt
+++ src/main/kotlin/configurations/JacksonConfiguration.kt
@@ -7,18 +7,17 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.messaging.converter.MappingJackson2MessageConverter
 
-//@Configuration
+@Configuration
 class JacksonConfig {
 
-//    @Bean
-//    fun objectMapper(): ObjectMapper =
-//        jacksonObjectMapper().registerKotlinModule()
-//
-//    @Bean
-//    fun mappingJackson2MessageConverter(objectMapper: ObjectMapper): MappingJackson2MessageConverter {
-//        val converter = MappingJackson2MessageConverter()
-//        converter.objectMapper = objectMapper
-//        converter.isStrictContentTypeMatch = false
-//        return converter
-//    }
+    @Bean
+    fun objectMapper(): ObjectMapper =
+        jacksonObjectMapper().registerKotlinModule()
+
+    @Bean
+    fun mappingJackson2MessageConverter(objectMapper: ObjectMapper): MappingJackson2MessageConverter {
+        val converter = MappingJackson2MessageConverter()
+        converter.objectMapper = objectMapper
+        return converter
+    }
 }
